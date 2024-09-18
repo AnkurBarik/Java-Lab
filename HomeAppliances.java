4685bcfab6ec3d3e04fee7630d479214ea972fd3
Ankur-Barik <154061998+Ankur-Barik@users.noreply.github.com>
2024-09-18 10:42:20 +0530

    Update HomeAppliances.java

--- HomeAppliances.java
+++ HomeAppliances.java
@@ -1,1 +1,29 @@
 
+class Appliance{
+  void operate(){
+    System.out.println("Inside Superclass Appliance");
+  }
+}
+
+class WashingMachines extends Appliance{
+  void operate(){
+    System.out.println("Inside Superclass WashingMachines");
+  }
+}
+
+class Refrigerator extends Appliance{
+  void operate(){
+    System.out.println("Inside Superclass Refrigerator");
+  }
+}
+
+public class HomeAppliance{
+  public static void main(String[] args){
+    Appliance app = new Appliance();
+    app.operate();
+    WashingMachines wash = new WashingMachines();
+    wash.operate();
+    Refrigerator ref = new Refrigerator();
+    ref.operate();
+  }
+}
