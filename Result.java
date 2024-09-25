d0e246c365450f620398bf833f40514d590c4e40
Ankur-Barik <154061998+Ankur-Barik@users.noreply.github.com>
2024-09-25 16:14:08 +0530

    Update Result.java

--- Result.java
+++ Result.java
@@ -1,35 +1,42 @@
-interface Exam{
+interface Exam {
   void percent_cal();
 }
-class Student{
+
+class Student {
   String name;
   int roll_no;
-  Student(String name, int roll_no){
+  
+  Student(String name, int roll_no) {
     this.name = name;
     this.roll_no = roll_no;
   }
-  void show(){
+  
+  void show() {
     System.out.println("Name: " + name);
     System.out.println("Roll No: " + roll_no);
   }
 }
 
-public class Result extends Student implements Exam{
+public class Result extends Student implements Exam {
   double marks;
-  Result(String name, int roll_no, int marks){
+  
+  Result(String name, int roll_no, double marks) {
     super(name, roll_no);
     this.marks = marks;
   }
-  void show(){
+  
+  void show() {
     super.show();
     System.out.println("Marks: " + marks);
   }
-  public void percent_cal(){
-    double percent = (marks/700)*100;
+  
+  public void percent_cal() {
+    double percent = (marks / 700) * 100;
     System.out.println("Percentage: " + percent);
   }
-  public static void main(String[] args){
-    Result r = new Result("Ankur", 055, 513);
+  
+  public static void main(String[] args) {
+    Result r = new Result("Ankur", 55, 513);
     r.show();
     r.percent_cal();
   }
