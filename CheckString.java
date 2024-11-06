d9c1347fc65f1a01cdd1192a6d276d91c7b7a6c2
Ankur Barik <154061998+AnkurBarik@users.noreply.github.com>
2024-11-06 12:23:30 +0530

    .

---
+++ CheckString.java
@@ -0,0 +1,22 @@
+class NoMatchFound extends Exception{
+  String err;
+  public NoMatchFound(String message){
+    this.err=message;
+  }
+}
+
+public class CheckString{
+  public static void main(String[] args){
+    try{
+      String inputString = args[0];
+      if (!inputString.equals("University")) {
+        throw new NoMatchFound("String does not match 'University'");
+      }
+      System.out.println("String matches 'University'");
+    } catch (NoMatchFound e) {
+      System.out.println("Error: " + e.err); 
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Error: Please provide a string as input.");
+    }
+  }
+}
